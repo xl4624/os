028afde00029fe68dc70c0a52de3805677ab2806
Xiaomin Liu <xl4624@nyu.edu>
2024-12-21 22:35:54 -0500

    Hello kernel

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+set -xe
+
+i686-elf-as boot.s -o boot.o
+i686-elf-g++ -c kernel.cpp -o kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
+i686-elf-g++ -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+
+if grub-file --is-x86-multiboot myos.bin; then
+  echo "multiboot confirmed"
+else
+  echo "NOT MULTIBOOT"
+fi
+
+mkdir -p isodir/boot/grub
+cp myos.bin isodir/boot/myos.bin
+cp grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o myos.iso isodir

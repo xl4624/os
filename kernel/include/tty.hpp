33f62a117c6ff5813ec80a00ac608a9889432a78
Xiaomin Liu <xl4624@nyu.edu>
2024-12-22 16:47:09 -0500

    Split VGA and tty

---
+++ kernel/include/tty.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+class Terminal {
+   public:
+    Terminal();
+    void putChar(char c);
+    void write(const char *data, size_t size);
+    void writeString(const char *data);
+
+   private:
+    void setColor(uint8_t color);
+    void putEntryAt(char c, uint8_t color, size_t x, size_t y);
+
+    size_t row_;
+    size_t column_;
+    uint8_t color_;
+    uint16_t *buffer_;
+};

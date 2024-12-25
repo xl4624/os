71e0e3323e4495d49c05f7e1e743887b147f83bd
Xiaomin Liu <xl4624@nyu.edu>
2024-12-24 19:50:54 -0500

    Fix linking errors with VGA::MEMORY

--- kernel/tty.cpp
+++ kernel/tty.cpp
@@ -21,6 +21,32 @@ Terminal::Terminal()
     }
 }
 
+void Terminal::writeString(const char *data) {
+    write(data, strlen(data));
+}
+
+void Terminal::write(const char *data, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        putChar(data[i]);
+    }
+}
+
+void Terminal::putChar(char c) {
+    if (c == '\n') {
+        column_ = 0;
+        if (++row_ == VGA::HEIGHT) {
+            scroll();
+        }
+    } else {
+        putEntryAt(c, color_, column_, row_);
+        if (++column_ == VGA::WIDTH) {
+            if (++row_ == VGA::HEIGHT) {
+                scroll();
+            }
+        }
+    }
+}
+
 void Terminal::setColor(uint8_t color) {
     color_ = color;
 }
@@ -42,32 +68,6 @@ void Terminal::scroll() {
     for (size_t col = 0; col < VGA::WIDTH; col++) {
         putEntryAt(' ', color_, col, row_);
     }
-}
-
-void Terminal::putChar(char c) {
-    if (c == '\n') {
-        column_ = 0;
-        if (++row_ == VGA::HEIGHT) {
-            scroll();
-        }
-    } else {
-        putEntryAt(c, color_, column_, row_);
-        if (++column_ == VGA::WIDTH) {
-            if (++row_ == VGA::HEIGHT) {
-                scroll();
-            }
-        }
-    }
-}
-
-void Terminal::write(const char *data, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        putChar(data[i]);
-    }
-}
-
-void Terminal::writeString(const char *data) {
-    write(data, strlen(data));
 }
 
 // ===============================

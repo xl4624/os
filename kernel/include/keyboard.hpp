4e55bc3065b882f3c3a6555d8cdb4eb2eb4af6cf
Xiaomin Liu <xl4624@nyu.edu>
2025-01-01 17:29:31 -0500

    Support shift in keyboard driver

--- kernel/include/keyboard.hpp
+++ kernel/include/keyboard.hpp
@@ -1,3 +1,16 @@
 #pragma once
 
-void keyboard_init();
+#include <stddef.h>
+#include <stdint.h>
+
+class KeyboardDriver {
+   public:
+    KeyboardDriver();
+    void handle_scancode(uint8_t scancode);
+
+   private:
+    uint8_t buffer_[256];
+    bool shift_;
+};
+
+extern KeyboardDriver keyboard;

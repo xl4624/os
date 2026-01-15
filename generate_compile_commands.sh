fab7e7aeaf25535f4f0b8329ac9622085ced99c0
Xiaomin Liu <xl4624@nyu.edu>
2026-01-15 13:47:35 -0800

    Remove compile_commands.json and update script

--- generate_compile_commands.sh
+++ generate_compile_commands.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Generate compile_commands.json for clangd
 
-set -ex
+set -e
 
 echo "Generating compile_commands.json..."
 
 make clean
 bear -- make
 bear --append -- make test
+# ktest iso file
+bear --append -- make myos-test.iso
 
 echo "Created compile_commands.json with $(grep -c '"file"' compile_commands.json) entries"

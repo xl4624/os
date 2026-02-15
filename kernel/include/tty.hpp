12cba398bc8d690480dd906c809b6eabc99a1456
Xiaomin Liu <xl4624@nyu.edu>
2026-02-14 20:19:26 -0800

    Add VGA hardware cursor

--- kernel/include/tty.hpp
+++ kernel/include/tty.hpp
@@ -17,18 +17,24 @@ class Terminal {
     void put_char(char c);
     void handle_key(KeyEvent event);
     void set_color(uint8_t color);
+    void enable_cursor();
+    void disable_cursor();
+    void update_cursor();
 
    private:
     void put_entry_at(char c, uint8_t color, size_t row, size_t col);
     void scroll();
     void clear_line(size_t row);
+    void move_cursor(size_t row, size_t col);
+
+    bool cursor_enabled_ = false;
 
     size_t row_ = 0;
     size_t col_ = 0;
     uint8_t color_ = VGA::entry_color(VGA::Color::LightGrey, VGA::Color::Black);
     uint8_t cursor_color_ =
         VGA::entry_color(VGA::Color::Black, VGA::Color::LightGrey);
-    volatile uint16_t *buffer_ = VGA::MEMORY;
+    volatile uint16_t *buffer_ = VGA::MEMORY_ADDR;
 };
 
 extern Terminal terminal;

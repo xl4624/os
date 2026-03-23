ccd955c6e7526ba7526953a0a903b39de6644482
Xiaomin Liu <xl4624@nyu.edu>
2026-03-22 23:16:55 -0700

    Consolidate build flags into config.mk

--- config.mk
+++ config.mk
@@ -8,11 +8,20 @@ AR       := $(CROSS)-ar
 
 # ==== Directory Structure ====
 SYSROOT  ?= $(CURDIR)/sysroot
+BUILDDIR ?= $(CURDIR)/build
 PREFIX     := /usr
 INCLUDEDIR := $(PREFIX)/include
 LIBDIR     := $(PREFIX)/lib
 
-# ==== Common Flags ====
-BASE_CFLAGS := --sysroot=$(SYSROOT) -isystem $(SYSROOT)/usr/include/c -O2 -g \
-               -ffreestanding -Wall -Wextra -Wpedantic -Wconversion -MMD -MP
-BASE_CXXFLAGS := -std=gnu++20 -isystem $(SYSROOT)/usr/include/cpp
+# ==== Kernel Build Flags ====
+KERNEL_CFLAGS := --sysroot=$(SYSROOT) -isystem $(SYSROOT)/usr/include/c -O2 -g \
+                 -ffreestanding -Wall -Wextra -Wpedantic -Wconversion -MMD -MP
+KERNEL_CXXFLAGS := $(KERNEL_CFLAGS) -std=gnu++20 -isystem $(SYSROOT)/usr/include/cpp
+
+# ==== Userspace Build Flags ====
+LIBC_CFLAGS := $(KERNEL_CFLAGS) -D__is_libc
+LIBC_CXXFLAGS := $(KERNEL_CXXFLAGS) -D__is_libc
+
+# ==== Unit Test Flags (host compiler) ====
+TEST_CFLAGS := -std=c11 -Wall -Wextra -D__is_libc
+TEST_CXXFLAGS := -std=gnu++20 -fconcepts -Wall -Wextra -D__is_libc
